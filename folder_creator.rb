eba4b021436a07eae8986219280e8400d0788d6d
Carlos Diaz <cdiaz86@gmail.com>
2019-09-22 21:58:18 -0400

    Adds ability to copy file into every directory.

--- folder_creator.rb
+++ folder_creator.rb
@@ -6,6 +6,7 @@ students = {"00000000" => "Reed Simmons", "00000001" => "Kyle Sampers", "0000000
 def folders(students)
   students.each { |key, value|
     Dir.mkdir "#{value} #{key}"
+    FileUtils.cp 'test.txt', "#{value} #{key}/test.txt"
   }
 end
 
