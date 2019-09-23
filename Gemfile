934745c8faf0b1ff61a7eae8860c7d3a403e0e47
Carlos Diaz <cdiaz86@gmail.com>
2019-09-22 21:56:08 -0400

    Adds ability to create folder based off name & ID number.

---
+++ Gemfile
@@ -0,0 +1,3 @@
+source 'https://rubygems.org'
+
+gem 'fileutils', '~> 0.7.2'
---
+++ folder_creator.rb
@@ -0,0 +1,12 @@
+require 'fileutils'
+
+students = {"00000000" => "Reed Simmons", "00000001" => "Kyle Sampers", "00000002" => "Lance Taylor", "00000003" => "Ryan Torver"}
+
+
+def folders(students)
+  students.each { |key, value|
+    Dir.mkdir "#{value} #{key}"
+  }
+end
+
+p folders(students)
