1c60bad03099dcaf51114692d9486dc4ee6453e3
Carlos Diaz <cdiaz86@gmail.com>
2019-09-24 12:40:39 -0400

    Adds info csv & identifies Student, ID, & Term Code Admit.

--- Gemfile
+++ Gemfile
@@ -1,3 +1,4 @@
 source 'https://rubygems.org'
 
 gem 'fileutils', '~> 0.7.2'
+gem 'csv'
--- folder_creator.rb
+++ folder_creator.rb
@@ -1,16 +1,26 @@
 require 'fileutils'
+require 'csv'
 
-students = {"00000000" => "Reed Simmons", "00000001" => "Kyle Sampers", "00000002" => "Lance Taylor", "00000003" => "Ryan Torver"}
-
-
-def folders(students)
-  students.each { |id, name|
-    name = name.split(" ")
-    first_name = name[0]
-    last_name = name[-1]
-    Dir.mkdir "#{last_name}-#{first_name}-#{id}"
-    FileUtils.cp 'test.txt', "#{last_name}-#{first_name}-#{id}/#{last_name}_#{first_name}_#{id}_UG_plan_of_study.txt"
-  }
+admit_201915 = []
+CSV.foreach('data/STU-Eligible_To_Register-Fall-92419-edited.csv', headers: true) do |header|
+  if header["Term Code Admit"] == "201915"
+    puts header["Student"]
+    puts header["Univ Id"]
+    puts header["Term Code Admit"]
+  end
 end
 
-p folders(students)
+# students = {"00000000" => "Reed Simmons", "00000001" => "Kyle Sampers", "00000002" => "Lance Taylor", "00000003" => "Ryan Torver"}
+
+
+# def folders(students)
+#   students.each { |id, name|
+#     name = name.split(" ")
+#     first_name = name[0]
+#     last_name = name[-1]
+#     Dir.mkdir "#{last_name}-#{first_name}-#{id}"
+#     FileUtils.cp 'master-2018+ plan of study.xlsx', "#{last_name}-#{first_name}-#{id}/#{last_name}_#{first_name}_#{id}_UG_plan_of_study.txt"
+#   }
+# end
+
+# p folders(students)
