8cf1a5d093b5c09bb16664adec7ed291e02f07df
Carlos Diaz <cdiaz86@gmail.com>
2019-09-24 13:19:19 -0400

    Corrects output to show last name, first name - ID.

--- folder_creator.rb
+++ folder_creator.rb
@@ -1,26 +1,29 @@
 require 'fileutils'
 require 'csv'
 
-admit_201915 = []
+admit_201915_array = []
+
 CSV.foreach('data/STU-Eligible_To_Register-Fall-92419-edited.csv', headers: true) do |header|
   if header["Term Code Admit"] == "201915"
-    puts header["Student"]
-    puts header["Univ Id"]
-    puts header["Term Code Admit"]
+    admit_201915_array << header["Univ Id"]
+    admit_201915_array << header["Student"]
+    
+    # puts header["Student"]
+    # puts header["Univ Id"]
   end
 end
 
-# students = {"00000000" => "Reed Simmons", "00000001" => "Kyle Sampers", "00000002" => "Lance Taylor", "00000003" => "Ryan Torver"}
+puts admit_201915 = Hash[*admit_201915_array]
 
 
-# def folders(students)
-#   students.each { |id, name|
-#     name = name.split(" ")
-#     first_name = name[0]
-#     last_name = name[-1]
-#     Dir.mkdir "#{last_name}-#{first_name}-#{id}"
-#     FileUtils.cp 'master-2018+ plan of study.xlsx', "#{last_name}-#{first_name}-#{id}/#{last_name}_#{first_name}_#{id}_UG_plan_of_study.txt"
-#   }
-# end
+def folders(admit_201915)
+  admit_201915.each { |id, name|
+    name = name.split(", ")
+    first_name = name[-1]
+    last_name = name[0]
+    Dir.mkdir "#{last_name}-#{first_name}-#{id}"
+    FileUtils.cp 'master-2018+ plan of study.xlsx', "#{last_name}-#{first_name}-#{id}/#{last_name}_#{first_name}_#{id}_UG_plan_of_study.xlsx"
+  }
+end
 
-# p folders(students)
+p folders(admit_201915)
